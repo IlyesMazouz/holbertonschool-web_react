b3de761203fd3f1a06529ffbb870dae70bfecf1b
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-09-19 19:34:55 +0200

    school web app

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="header-title">School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p className="body-text">Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+
+        <button type="button">OK</button>
+      </main>
+      <footer className="App-footer">
+        <p className="footer-text">
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
