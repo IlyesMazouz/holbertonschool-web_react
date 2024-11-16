8754883bcc12c20ad16a87838a651069a8c94e28
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-11-16 19:01:59 +0100

    app file fix

--- react_component/task_0/dashboard/src/App/App.jsx
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -1,57 +1,25 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './App.css';
-import Login from '../Login/Login';
 import Header from '../Header/Header';
-import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
 import Footer from '../Footer/Footer';
+import './App.css';
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listCourses: [
-        { id: 1, name: 'ES6', credit: 60 },
-        { id: 2, name: 'Webpack', credit: 20 },
-        { id: 3, name: 'React', credit: 40 }
-      ],
-      listNotifications: [
-        { id: 1, type: 'default', value: 'New course available' },
-        { id: 2, type: 'urgent', value: 'New resume available' },
-        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
-      ]
-    };
-
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown(event) {
-    if (event.ctrlKey && event.key === 'h') {
-      event.preventDefault();
-      alert('Logging you out');
-      this.props.logOut();
-    }
-  }
-
   render() {
     const { isLoggedIn } = this.props;
-    const { listCourses, listNotifications } = this.state;
-
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
     return (
       <>
         <div className="App">
-          <Notifications listNotifications={listNotifications}/>
           <Header />
-          <div className="App-body">
+          <div className="app-body">
             {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
           </div>
           <Footer />
@@ -62,13 +30,7 @@ class App extends Component {
 }
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
-};
-
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => {}
+  isLoggedIn: PropTypes.bool.isRequired,
 };
 
 export default App;
