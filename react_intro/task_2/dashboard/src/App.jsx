c25842c72b74c7b91973ee056000269b0822690f
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-19 18:09:54 +0200

    adding lables for pw and email and ok button

---
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css';
+import logo from './assets/holberton-logo.jpg';
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+import './Notifications.css';
+
+function App() {
+  const year = getCurrentYear();
+  const footerText = getFooterCopy(false); 
+
+  return (
+    <>
+    <div className="root-notifications">
+        <Notifications />
+      </div>
+
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright {year} - {footerText}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
