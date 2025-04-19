61b75f3e79194344063991b0527ddbf7398ee4c8
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-19 17:57:03 +0200

    fixing app file

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
 import './App.css';
 import logo from './assets/holberton-logo.jpg';
-import { getCurrentYear, getFooterCopy } from './utils';
 import Notifications from './Notifications';
 
 function App() {
+  const year = new Date().getFullYear();
+
   return (
     <>
-      <div className="root-notifications">
+    <div className="root-notifications">
         <Notifications />
       </div>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} alt="Holberton logo" />
-          <h1>School dashboard</h1>
-        </div>
-        <div className="App-body">
-          <p>Login to access the full dashboard</p>
-        </div>
-        <div className="App-footer">
-          <p>
-            Copyright {getCurrentYear()} - {getFooterCopy(true)}
-          </p>
-        </div>
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {year} - holberton School</p>
       </div>
     </>
   );
