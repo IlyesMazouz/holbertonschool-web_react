71374639c681a9e32b71720ef0156984a82d1149
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-19 17:24:18 +0200

    fix

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/holberton-logo.jpg';
-import Notifications from './Notifications';
 import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
 
 function App() {
+  const isIndex = false; 
+
   return (
     <>
       <div className="root-notifications">
         <Notifications />
       </div>
-      <div className="App-header">
-        <img src={logo} alt="holberton logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-      <div className="App-footer">
-        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} alt="holberton logo" />
+          <h1>School dashboard</h1>
+        </div>
+        <hr />
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+        </div>
+        <div className="App-footer">
+          <p>
+            Copyright {getCurrentYear()} - {getFooterCopy(isIndex)}
+          </p>
+        </div>
       </div>
     </>
   );
