3b3a46f014807ee2b7f67e8ce232fea9651e3826
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-21 20:52:33 +0200

    fix

--- react_props/task_0/dashboard/src/Login/Login.jsx
+++ react_props/task_0/dashboard/src/Login/Login.jsx
@@ -5,15 +5,11 @@ function Login() {
   return (
     <div className="App-body">
       <p>Login to access the full dashboard</p>
-      <form>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" autoComplete="email" />
-
+        <input type="email" id="email" name="email" />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" autoComplete="current-password" />
-
+        <input type="password" id="password" name="password"/>
         <button>OK</button>
-      </form>
     </div>
   );
 }
