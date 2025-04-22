e4e6f13308bf95d93b0da735521cbcdf83a1300b
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-22 22:55:42 +0200

    revert

--- react_props/task_2/dashboard/eslint.config.js
+++ react_props/task_2/dashboard/eslint.config.js
@@ -1,8 +1,7 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import eslintPluginJest from 'eslint-plugin-jest';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   { ignores: ['dist'] },
@@ -31,18 +30,4 @@ export default [
       ],
     },
   },
-  {
-    files: ['**/*.spec.js'],
-    languageOptions: {
-      globals: {
-        ...globals.jest, 
-      },
-    },
-    plugins: {
-      jest: eslintPluginJest,
-    },
-    rules: {
-      ...eslintPluginJest.configs.recommended.rules,
-    },
-  },
-];
+]
