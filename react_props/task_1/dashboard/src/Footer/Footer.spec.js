87d7e897bb4e0465a367369a33cf84df540484cc
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-22 19:50:49 +0200

    updating footer test file

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
-import { getFooterCopy } from '../utils/utils';
+import * as utils from '../utils/utils';
 
 describe('Footer component', () => {
-  test('renders correct copyright', () => {
+  test('renders correct copyright when getFooterCopy is called with isIndex = true', () => {
+    jest.spyOn(utils, 'getFooterCopy').mockReturnValue('Holberton School');
+    
     render(<Footer />);
+
     const year = new Date().getFullYear();
     const expectedText = `Copyright ${year} - Holberton School`;
     expect(screen.getByText(expectedText)).toBeInTheDocument();
