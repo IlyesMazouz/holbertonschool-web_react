c1c68121d0814ab9419dec7c3de54abc6fcf8fd3
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-22 22:27:20 +0200

    fixing the urgent msg in notifications

--- react_props/task_2/dashboard/src/App/App.jsx
+++ react_props/task_2/dashboard/src/App/App.jsx
@@ -3,17 +3,14 @@ import Notifications from "../Notifications/Notifications";
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import { getLatestNotification } from '../utils/utils';
 import './App.css';
 
 function App() {
   const notificationsList = [
     { id: 1, type: 'default', value: 'New course available' },
     { id: 2, type: 'urgent', value: 'New resume available' },
-    {
-      id: 3,
-      type: 'urgent',
-      html: '<strong>Urgent notification</strong>',
-    },
+    { id: 3, type: 'urgent', html: getLatestNotification() },
   ];
 
   return (
