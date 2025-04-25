08c7ed6c3e7a31fe8c308e560c85a6c2181995cf
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-25 17:08:40 +0200

    switching to hex value styling

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.spec.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.spec.js
@@ -6,18 +6,18 @@ describe('CourseListRow Component', () => {
   it('renders one cell with colspan = 2 when isHeader is true and textSecondCell is null', () => {
     const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="Test" />);
     const tr = wrapper.find('tr');
-    expect(tr.prop('style')).toHaveProperty('backgroundColor', 'rgba(222, 181, 181, 0.27)');
+    expect(tr.prop('style')).toHaveProperty('backgroundColor', '#deb5b545');
   });
 
   it('renders two th elements when isHeader is true and textSecondCell is provided', () => {
     const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First" textSecondCell="Second" />);
     const tr = wrapper.find('tr');
-    expect(tr.prop('style')).toHaveProperty('backgroundColor', 'rgba(222, 181, 181, 0.27)');
+    expect(tr.prop('style')).toHaveProperty('backgroundColor', '#deb5b545');
   });
 
   it('renders two td elements with correct background color when isHeader is false', () => {
     const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="First" textSecondCell="Second" />);
     const tr = wrapper.find('tr');
-    expect(tr.prop('style')).toHaveProperty('backgroundColor', 'rgba(245, 245, 245, 0.67)');
+    expect(tr.prop('style')).toHaveProperty('backgroundColor', '#f5f5f5ab');
   });
 });
