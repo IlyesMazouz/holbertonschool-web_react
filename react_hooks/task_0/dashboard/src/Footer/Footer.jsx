884681cfc9a16c6470d802cbed10a284130e52a3
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-27 16:36:10 +0200

    updating footer component

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -4,12 +4,19 @@ import { getCurrentYear, getFooterCopy } from '../utils/utils';
 import { newContext } from '../Context/context';
 
 function Footer() {
-  const { user } = useContext(newContext);
+  const { user, logOut } = useContext(newContext);
 
   return (
     <div className="App-footer">
       <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
-      {user?.isLoggedIn && (
+      {user?.isLoggedIn ? (
+        <>
+          <p>Welcome {user.email}</p>
+          <p>
+            <a href="#" onClick={logOut}>Logout</a>
+          </p>
+        </>
+      ) : (
         <p>
           <a href="#">Contact us</a>
         </p>
