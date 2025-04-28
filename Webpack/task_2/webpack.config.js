571ac2d37739aea812307c52775c0bd929dc0549
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-04-28 21:57:38 +0200

    webpack.config.js file

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    assetModuleFilename: 'assets/[name][ext][query]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  mode: 'production',
+};
